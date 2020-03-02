e7f121ffbf3edafe0ac74d653046d160dbd2b43a
LeanAlvarez11 <59622984+LeanAlvarez11@users.noreply.github.com>
2020-03-02 17:59:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var canvas = document.querySelector ("#canvas");
 canvas.width= 600
 canvas.height = 500
 var ctx = canvas.getContext('2d');
-var aux = {}
+var aux = {};
+var auxw = {};
 var wachin= {
 	hp:100,
 	rows: 1, 
@@ -20,7 +21,22 @@ var wachin= {
 	srcY:0, 
 	image: new Image(), 
 }
-
+var ataqueEspada= {
+	hp:100,
+	rows: 1, 
+	cols:5, 
+	spriteWidth : 2834, 
+	spriteHeight : 380,
+	width: 2834/5,
+	height: 380/1, 
+	curFrame : 0,
+	frameCount : 5, 
+	x:50,
+	y: (canvas.height /2 ) -150,
+	srcX:0,
+	srcY:0, 
+	image: new Image(), 
+}
 var malo= {
 	hp:100,
 	rows: 1, 
@@ -41,6 +57,22 @@ var golpeEnemigo = {
 	hp:0,
 	rows: 1, 
 	cols: 5, 
+	spriteWidth : 1878, 
+	spriteHeight : 350,
+	width: 1878/6, 
+	height: 350/1, 
+	curFrame : 0,
+	frameCount : 6, 
+	x:300,
+	y: (canvas.height /2 ) -150,
+	srcX:0,
+	srcY:0, 
+	image: new Image(), 
+}
+var danioEnemigo = {
+	hp:0,
+	rows: 1, 
+	cols: 6, 
 	spriteWidth : 1565, 
 	spriteHeight : 350,
 	width: 1565/5, 
@@ -53,6 +85,13 @@ var golpeEnemigo = {
 	srcY:0, 
 	image: new Image(), 
 }
+var corazones=[];
+var corazonesEnemigos=[];
+var corazon;
+var corazonEnemigo;
+var juego="iniciando";
+var sonidoEspada;
+var sonidoInterferencia;
 var score=0;
 var timer=10;
 var intervalo;
@@ -62,7 +101,6 @@ intervalo=setInterval(function(){
 	timer--;
 },1000)
 }
-
 document.getElementById("texto").style="display:none"
 document.getElementById("preg").style="display:none"
 document.getElementById("canvasDiv").style="display:none"
@@ -132,14 +170,24 @@ async function loadQuestions(valueRadio){
 var rc;
 function loadMedia (){
 	fondo= new Image();
+	corazon=new Image();
+	corazon.src="logo.png";
+	corazonEnemigo= new Image();
+	corazonEnemigo.src="logo.png"
 	fondo.src="fond.jpg";
 	wachin.image.src = "theBoy3.png";
 	malo.image.src = "spi.png";
-	golpeEnemigo.image.src = "ataqueEnemigo.png"
+	golpeEnemigo.image.src = "ataqueEnemigo.png";
+	danioEnemigo.image.src = "danioEnemigo.png";
+	ataqueEspada.image.src = "espadazo.png";
+	sonidoEspada = document.createElement("audio")
+	sonidoInterferencia = document.createElement("audio")
+	sonidoEspada.setAttribute("src","sounds/sword.mp3")
+	sonidoInterferencia.setAttribute("src","sounds/interference.mp3")
 	document.getElementById("preg").style="display:inline";
 	document.getElementById("canvasDiv").style="display:inline"
 	fondo.onload= function(){
-		var intervalo = window.setInterval(frameloop,1000/3);}
+		var intervalo = window.setInterval(frameloop,1000/5);}
 	}
 	function dibujarFondo (){
 		ctx.drawImage (fondo,0,0,600,500);
@@ -149,34 +197,90 @@ function loadMedia (){
 		ctx.fillText("WACHIN HP: " +wachin.hp,100,40);
 		ctx.fillText("OPA HP: " +malo.hp,420,40);
 	}
-	function pinia(){
-	//animacionPiña
-	wachin.width=300
-	setTimeout(function (){
-		wachin.width=900/4
+	function actualizaCorazones(){
+		if (juego=="iniciando"){
+			for ()
+		}
+	}
+	function dibujarCorazones(){
+		let x=10;
+		let y=40;
+		let width=25;
+		let height=25;
+		for (i=0;i<10;i++){
+			let enemigo = corazones[i]; 
+			ctx.save();
+			ctx.drawImage(corazon,x,y,width,height);
+			x += 25;
+		}
+	}
+	function dibujarCorazonesEnemigos(){
+		let x=330;
+		let y=40;
+		let width=25;
+		let height=25;
+		for (i=0;i<10;i++){
+			let enemigo = corazonesEnemigos[i]; 
+			ctx.save();
+			ctx.drawImage(corazonEnemigo,x,y,width,height);
+			x += 25;
+		}
+	}
+function pinia(){
+	sonidoEspada.play()
+	ataqueEspada.hp=wachin.hp
+	auxw=wachin
+	wachin=ataqueEspada
+	setTimeout(function(){
+		wachin=auxw
 	},1000)
 }
 function patada(){
-	//animacionPatada
-	wachin.y=500
-	setTimeout(function (){
-		wachin.y=(canvas.height /2 ) -150
+	sonidoEspada.play()
+	ataqueEspada.hp=wachin.hp
+	auxw=wachin
+	wachin=ataqueEspada
+	setTimeout(function(){
+		wachin=auxw
 	},1000)
 }
 function espada(){
-	wachin.image.src="ataqueEnemigo.png";
-		setTimeout(function (){
-		wachin.image.src="theBoy3.png";
+	sonidoEspada.play()
+	ataqueEspada.hp=wachin.hp
+	auxw=wachin
+	wachin=ataqueEspada
+	setTimeout(function(){
+		wachin=auxw
 	},1000)
 }
 function cabeza(){
-	//animacionCabeza
-	wachin.x=400
-	setTimeout(function (){
-		wachin.x=50
+	sonidoEspada.play()
+	ataqueEspada.hp=wachin.hp
+	auxw=wachin
+	wachin=ataqueEspada
+	setTimeout(function(){
+		wachin=auxw
 	},1000)
 }
+function danioEnemigoo(){
+	sonidoInterferencia.play()
+	malo.hp-=10
+	danioEnemigo.hp=malo.hp
+	aux=malo
+	malo=danioEnemigo
+	setTimeout(function(){
+	sonidoInterferencia.pause()
+	malo=aux
+	},1500)
+}
 function enemii(){
+	racha=0;
+	if (score>=20){
+		score-=20;
+	}
+	else{
+		score=0;
+	}
 	golpeEnemigo.hp=malo.hp
 	aux=malo
 	malo=golpeEnemigo
@@ -185,10 +289,16 @@ function enemii(){
 	},1500)
 	wachin.hp-=10
 }
-function jugando(){
-	if (timer<=0){
-		comprobar()		
-		jugar();
+ function jugando(){
+	if (timer<0){
+		document.getElementById("res").style.display ="none"
+		timer = 10
+		enemii()
+		 setTimeout(function(){
+			jugar(yaUsado)
+		}, 1000)
+		
+		
 	}
 	if (juego.estado=="ganaste"){
 		document.getElementById("texto").style="display:inline"
@@ -211,11 +321,10 @@ function jugando(){
 		document.getElementById("timer").style="display:none"	
 	}
 }
-function dibujarHp(){
-
-}
 function frameloop(){
 	dibujarFondo();
+	dibujarCorazones()
+	dibujarCorazonesEnemigos()
 	draw(wachin);
 	draw(malo)
 	// drawEnemi();
@@ -262,36 +371,32 @@ function jugar(asd){
 	}
 }
 function comprobar(respuesta){
-	if(respuesta==rc && timer>0){
+	if (timer>0){
+	if(respuesta==rc){
 		score+=10*(timer+racha);
 		racha++
 		if (respuesta==0){
 			pinia();
-			malo.hp-=10
+			danioEnemigoo();
 
 		}
 		else if (respuesta==1){
 			patada();
-			malo.hp-=10
+			danioEnemigoo();
 		}
 		else if (respuesta==2){
 			espada();
-			malo.hp-=10
+			danioEnemigoo();
+
 		}
 		else if (respuesta==3){
 			cabeza();
-			malo.hp-=10
+			danioEnemigoo();
 		}
 	}
-	else{
-		racha=0;
-		if (score>=20){
-			score-=20;
-		}
-		else{
-			score=0;
-		}
+	else {
 		enemii();
+	}
 	}
 	if (malo.hp<=0) {
 		juego.estado="ganaste";
@@ -299,11 +404,10 @@ function comprobar(respuesta){
 	else if (wachin.hp<=0){
 		juego.estado="perdiste";
 	}
-	document.getElementById("res").style="display:none"
+	document.getElementById("res").style.display ="none"
 	setTimeout(function (){
-	jugar(yaUsado);
-
-},1500)
+		jugar(yaUsado);
+	},1500)
 }
 document.getElementById("texto").style="display:none"
 document.getElementById("preg").style="display:none"
